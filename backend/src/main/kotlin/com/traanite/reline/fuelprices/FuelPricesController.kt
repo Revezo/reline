433ad5103e99c689054ecad233721d7770c66579
wukash.r <lukasz.m.roszkiewicz@gmail.com>
2023-05-25 19:29:08 +0200

    /fuelprices maps to DTO

--- backend/src/main/kotlin/com/traanite/reline/fuelprices/FuelPricesController.kt
+++ backend/src/main/kotlin/com/traanite/reline/fuelprices/FuelPricesController.kt
@@ -1,6 +1,6 @@
 package com.traanite.reline.fuelprices
 
-import com.traanite.reline.fuelprices.model.CountryFuelPriceData
+import com.traanite.reline.fuelprices.model.*
 import com.traanite.reline.fuelprices.services.FuelPricesService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -14,11 +14,33 @@ class FuelPricesController(private val fuelPricesService: FuelPricesService) {
 
     @GetMapping
     fun getStoredGasolinePrices(): Mono<FuelPricesResponse> {
-        return fuelPricesService.findAll().collectList().map { FuelPricesResponse(it) }
+        return fuelPricesService.findAll().collectList().map { toFuelPricesResponse(it) }
     }
+
+    private fun toFuelPricesResponse(fuelPricesData: List<CountryFuelPriceData>): FuelPricesResponse {
+        val pricesData = fuelPricesData.map {
+            CountryFuelPriceDataResponse(it.country.value,
+                toFuelPriceDataResponse(it.gasolineData),
+                toFuelPriceDataResponse(it.gasolineData))
+        }
+        return FuelPricesResponse(pricesData)
+    }
+
+    private fun toFuelPriceDataResponse(fuelPrice: FuelPrice) : FuelPriceResponse {
+        return when (fuelPrice) {
+            is SimpleFuelPrice -> FuelPriceResponse(fuelPrice.price, BigDecimal(-1), BigDecimal(-1))
+            is ComplexFuelPrice -> FuelPriceResponse(
+                fuelPrice.averagePrice,
+                fuelPrice.minimalPrice,
+                fuelPrice.maximalPrice
+            )
+            else -> FuelPriceResponse(BigDecimal(-1), BigDecimal(-1), BigDecimal(-1))
+        }
+    }
+
 }
 
-data class FuelPricesResponse(val values: List<CountryFuelPriceData>) {
+data class FuelPricesResponse(val values: List<CountryFuelPriceDataResponse>) {
 }
 
 data class CountryFuelPriceDataResponse(
@@ -28,15 +50,8 @@ data class CountryFuelPriceDataResponse(
 ) {
 }
 
-data class ComplexFuelPriceResponse(
+data class FuelPriceResponse(
     val averagePrice: BigDecimal,
     val minimalPrice: BigDecimal,
     val maximalPrice: BigDecimal
-) :
-    FuelPriceResponse(averagePrice) {
-}
-
-data class SimpleFuelPriceResponse(val averagePrice: BigDecimal) : FuelPriceResponse(averagePrice) {
-}
-
-abstract class FuelPriceResponse(averagePrice: BigDecimal)
+) {}
