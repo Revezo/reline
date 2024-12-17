4f15786293caee944cfa146810c3b35675d552e7
wukash-r <lukasz.m.roszkiewicz@gmail.com>
2024-12-17 20:03:35 +0100

    backend JVM and dependency updates

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,17 +1,19 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 
 plugins {
-	id("org.springframework.boot") version "3.2.3"
-	id("io.spring.dependency-management") version "1.1.4"
-	kotlin("jvm") version "1.9.22"
-	kotlin("plugin.spring") version "1.9.22"
+	id("org.springframework.boot") version "3.4.0"
+	id("io.spring.dependency-management") version "1.1.7"
+	kotlin("jvm") version "2.1.0"
+	kotlin("plugin.spring") version "2.1.0"
+	id("com.github.ben-manes.versions") version "0.51.0"
 }
 
 group = "com.traanite"
-version = "1.1.0" // todo replace with property
+version = "1.1.1" // todo replace with property
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_21
+	sourceCompatibility = JavaVersion.VERSION_23
 }
 
 repositories {
@@ -19,6 +21,13 @@ repositories {
 }
 
 dependencies {
+	val resilience4jVersion = "2.2.0"
+	val jsoupVersion = "1.18.3"
+	val guavaVersion = "33.4.0-jre"
+	val springDocOpenApiVersion = "2.7.0"
+	val slf4jVersion = "2.0.16"
+	val kotlinLoggingJvmVersion = "7.0.3"
+
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
 	implementation("org.springframework.boot:spring-boot-starter-security")
@@ -29,25 +38,24 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 
-	implementation("io.github.resilience4j:resilience4j-reactor:1.7.1")
-	implementation("io.github.resilience4j:resilience4j-ratelimiter:1.7.1")
-	implementation("org.jsoup:jsoup:1.17.2")
-	implementation("com.google.guava:guava:33.0.0-jre")
+	implementation("io.github.resilience4j:resilience4j-reactor:${resilience4jVersion}")
+	implementation("io.github.resilience4j:resilience4j-ratelimiter:${resilience4jVersion}")
+	implementation("org.jsoup:jsoup:${jsoupVersion}")
+	implementation("com.google.guava:guava:${guavaVersion}")
+	api("org.springdoc:springdoc-openapi-starter-webflux-ui:${springDocOpenApiVersion}")
 
-	api("org.springdoc:springdoc-openapi-starter-webflux-ui:2.3.0")
-
-	implementation("org.slf4j:slf4j-api:2.0.12")
-	implementation("io.github.oshai:kotlin-logging-jvm:6.0.3")
+	implementation("org.slf4j:slf4j-api:${slf4jVersion}")
+	implementation("io.github.oshai:kotlin-logging-jvm:${kotlinLoggingJvmVersion}")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.projectreactor:reactor-test")
 	testImplementation("org.springframework.security:spring-security-test")
 }
 
-tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs += "-Xjsr305=strict"
-		jvmTarget = "21"
+tasks.withType<KotlinJvmCompile>().configureEach {
+	compilerOptions {
+		jvmTarget.set(JvmTarget.JVM_23)
+		freeCompilerArgs.add("-Xjsr305=strict")
 	}
 }
 
