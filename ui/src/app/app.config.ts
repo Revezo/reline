15fe10877048eb1e2f346c2b71e3b5ab89c52f41
wukash-r <lukasz.m.roszkiewicz@gmail.com>
2024-12-17 18:41:10 +0100

    fixes after update

--- ui/src/app/app.config.ts
+++ ui/src/app/app.config.ts
@@ -1,10 +1,10 @@
-import {ApplicationConfig, importProvidersFrom} from '@angular/core';
+import {ApplicationConfig} from '@angular/core';
 import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
-import {} from "@angular/common/http";
+import {provideHttpClient, withInterceptorsFromDi} from "@angular/common/http";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync(), importProvidersFrom(HttpClientModule)]
+  providers: [provideRouter(routes), provideAnimationsAsync(), provideHttpClient(withInterceptorsFromDi())]
 };
