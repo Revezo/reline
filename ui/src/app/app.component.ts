2f61584bfb3509e9c8435870c99c91f54d454431
wukash-r <lukasz.m.roszkiewicz@gmail.com>
2024-12-17 18:27:14 +0100

    ng update angular 19

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -3,11 +3,10 @@ import { RouterOutlet } from '@angular/router';
 import {FuelPricesComponent} from "./fuelprices/fuelprices.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, FuelPricesComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    imports: [RouterOutlet, FuelPricesComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ui';
